f9edf424310914e2a370c7c1295992e0914c3329
ronypurwita <purwita.rony@gmail.com>
2017-03-16 16:42:26 +0700

    updated index view test

--- jmad/core/tests.py
+++ jmad/core/tests.py
@@ -14,6 +14,12 @@ class StudentTestCase(LiveServerTestCase):
 
     def test_student_find_solos(self):
         """Test that a student can search for solos"""
+        # Visit homepage of JMAD
+        homepage = self.browser.get(self.live_server_url + '/')
+
+        # JMAD shown in the homepage heading
         brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
         self.assertEqual('JMAD', brand_element.text)
+
+        # This test is incomplete
         self.fail('incomplete test')
