95bc8a28aef55d24a654509df0a543358a00fcb0
ronypurwita <purwita.rony@gmail.com>
2017-03-16 17:29:38 +0700

    added form elements, fake search result in index.html, updated functional tests

--- jmad/core/tests.py
+++ jmad/core/tests.py
@@ -21,5 +21,40 @@ class StudentTestCase(LiveServerTestCase):
         brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
         self.assertEqual('JMAD', brand_element.text)
 
+        # Search form, including label and placeholder is exists
+        # in homepage
+        instrument_input = self.browser.find_element_by_css_selector(
+            'input#jmad-instrument')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'label[for="jmad-instrument"]'))
+        self.assertEqual(instrument_input.get_attribute('placeholder'), 'i.e. trumpet')
+
+        artist_input = self.browser.find_element_by_css_selector(
+            'input#jmad-artist')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'label[for="jmad-artist"]'))
+        self.assertEqual(artist_input.get_attribute('placeholder'), 'i.e. Davis')
+
+        # Typing the name of the instrument and click the submit button
+        instrument_input.send_keys('saxophone')
+        self.browser.find_element_by_css_selector('form button').click()
+
+        # Too many search results, need to add a particular artist
+        # to search query
+        search_results = self.browser.find_elements_by_css_selector(
+            '.jmad-search-result')
+        self.assertGreater(len(search_results), 2)
+
+        # Adding a particular artist in search query and
+        # get more manageable result
+        second_artist_input = self.browser.find_element_by_css_selector(
+            'input#jmad-artist')
+        second_artist_input.send_keys('Cannonball Adderley')
+        self.browser.find_element_by_css_selector('form button').click()
+
+        second_search_result = self.browser.find_elements_by_css_selector(
+            '.jmad-search-result')
+        self.assertEqual(len(second_search_result), 2)
+
         # This test is incomplete
         self.fail('incomplete test')
