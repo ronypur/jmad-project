a4be36566952838c1df22f25d1af22987a93d480
ronypurwita <purwita.rony@gmail.com>
2017-03-17 23:04:09 +0700

    refactored test data with setUpClass in SolosBaseTestCase

--- jmad/apps/solos/tests/test_views.py
+++ jmad/apps/solos/tests/test_views.py
@@ -5,6 +5,46 @@ from apps.solos.models import Solo
 from apps.solos.views import index, SoloDetailView
 
 
+class SolosBaseTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    @classmethod
+    def setUpClass(cls):
+        super(SolosBaseTestCase, cls).setUpClass()
+        cls.drum_solo = Solo.objects.create(
+            instrument='drums',
+            artist='Rich',
+            track='Bugle Call Rag'
+        )
+        cls.sax_solo = Solo.objects.create(
+            instrument='saxophone',
+            artist='Coltrane',
+            track='Mr. PC'
+        )
+
+
+class IndexViewTestCase(SolosBaseTestCase):
+
+    def test_index_view_basic(self):
+        """Test index view returns a 200 response and uses the correct template"""
+        request = self.factory.get('/')
+        with self.assertTemplateUsed('solos/index.html'):
+            response = index(request)
+            self.assertEqual(response.status_code, 200)
+
+    def test_index_view_returns_solos(self):
+        """Test that the index view will return solos if query parameters exist"""
+        response = self.client.get('/', {'instrument': 'drums'})
+
+        solos = response.context['solos']
+
+        self.assertIs(type(solos), QuerySet)
+        self.assertEqual(len(solos), 1)
+        self.assertEqual(solos[0].artist, 'Rich')
+
+
 class SoloViewTestCase(TestCase):
 
     def setUp(self):
@@ -32,38 +72,3 @@ class SoloViewTestCase(TestCase):
 
         with self.assertTemplateUsed('solos/solo-detail.html'):
             response.reader()
-
-
-class IndexViewTestCase(TestCase):
-
-    def setUp(self):
-        self.factory = RequestFactory()
-
-        self.drum_solo = Solo.objects.create(
-            instrument='drums',
-            artist='Rich',
-            track='Bugle Call Rag'
-        )
-
-        self.bass_solo = Solo.objects.create(
-            instrument='bass',
-            artist='Coltrane',
-            track='Mr. PC'
-        )
-
-    def test_index_view_basic(self):
-        """Test index view returns a 200 response and uses the correct template"""
-        request = self.factory.get('/')
-        with self.assertTemplateUsed('solos/index.html'):
-            response = index(request)
-            self.assertEqual(response.status_code, 200)
-
-    def test_index_view_returns_solos(self):
-        """Test that the index view will return solos if query parameters exist"""
-        response = self.client.get('/', {'instrument': 'drums'})
-
-        solos = response.context['solos']
-
-        self.assertIs(type(solos), QuerySet)
-        self.assertEqual(len(solos), 1)
-        self.assertEqual(solos[0].artist, 'Rich')
